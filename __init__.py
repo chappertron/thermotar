a8338577fcac2dbd5abb6cd96b8df810aa197fc4
aidanium <52535659+chappertron@users.noreply.github.com>
2020-12-28 18:11:52 +0000

    Restructured so that the package has a setup.py file

--- __init__.py
+++
@@ -1,2 +0,0 @@
-from . import utils
-from .thermo import *
--- thermo_vars.py
+++
@@ -1,4 +0,0 @@
-'''This file lists the lammps default thermodynamic variables 
-and a sub list of 'time extensive' variables 
-used for plotting time series of properties in thermo objects'''
-
