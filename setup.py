b73ad39414a5742283e9c1a516bac3333dfa9001
chappertron <agchap@gmail.com>
2020-12-28 22:33:50 +0000

    readded the files i accidentally deleted..

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='thermotar',
+    version='0.0.1',
+    author='Aidan C',
+    packages=['thermotar'],
+    scripts=[],
+    url='',
+    description='An awesome package that does something',
+    long_description=open('README.md').read(),
+    install_requires=[
+       "pandas",
+       "numpy",
+   ],
+)
---
+++ thermotar/thermo_vars.py
@@ -0,0 +1,4 @@
+'''This file lists the lammps default thermodynamic variables 
+and a sub list of 'time extensive' variables 
+used for plotting time series of properties in thermo objects'''
+
