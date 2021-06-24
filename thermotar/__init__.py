8de012fb063a1e9b5403d8dc117db4abdf42fe70
aidanium <52535659+chappertron@users.noreply.github.com>
2021-06-24 16:01:08 +0100

    Commit changes

--- thermotar/__init__.py
+++ thermotar/__init__.py
@@ -2,3 +2,7 @@ from . import utils
 from .thermo import *
 from .chunk import *
 from .replicator import *
+
+### aliases for constructors
+create_thermos = Thermo.create_thermos
+create_chunk = Chunk.create_chunk
