baf2e313e0a72a2770e66105fe902c4bd34fd55a
chappertron <ac5116@ic.ac.uk>
2021-09-17 10:51:11 +0100

    No circ

--- thermotar/__init__.py
+++ thermotar/__init__.py
@@ -1,8 +1,8 @@
 #from . import utils
+import utils
 from .thermo import *
 from .chunk import *
 from .replicator import *
-import utils
 
 ### aliases for constructors
 create_thermos = Thermo.create_thermos
