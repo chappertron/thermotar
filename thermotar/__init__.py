ee53d76a720eccc778b37e570a3b210bb86298f0
chappertron <52535659+chappertron@users.noreply.github.com>
2023-10-15 17:37:21 +0100

    feat: added re-exports for useful functions buried in utils

--- thermotar/__init__.py
+++ thermotar/__init__.py
@@ -2,9 +2,16 @@
 from .thermo import Thermo
 from .chunk import Chunk
 from .multichunk import MultiChunk
-
+from .utils import df_utils
+from .utils import bash_like
 
 ### aliases for constructors
 create_thermos = Thermo.create_thermos
 create_chunk = Chunk.create_chunk
 create_multi_chunks = MultiChunk.create_multi_chunks
+
+### aliases for useful helper functions
+df_ave_and_err = df_utils.df_ave_and_err
+rebin = df_utils.rebin
+glob_to_re_group = bash_like.glob_to_re_group
+
