4b7c5f827a44431e32744df88041235de285d962
chappertron <52535659+chappertron@users.noreply.github.com>
2024-08-03 00:12:08 +0100

    Deleted usless test file

--- test_files/import_test.py
+++
@@ -1,8 +0,0 @@
-import thermotar as th
-print("imported thermotar")
-from thermotar import utils as thermo_utils
-print("imported Utils")
-from thermotar import thermo
-print("imported thermo")
-from thermotar import chunk
-print("imported chunk")
